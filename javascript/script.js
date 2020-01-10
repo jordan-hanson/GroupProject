3c6184aeec893fd7599c183423eaff9073f0f93b
Jacob Dolph <jacob.dolph702@gmail.com>
2020-01-10 14:33:03 -0700

    updating css

--- javascript/script.js
+++ javascript/script.js
@@ -46,8 +46,10 @@ const getCoffeeShops = () => {
             console.log(dataEl)
             for (var i = 0; i < dataEl.candidates.length; i++) {
                 console.log(dataEl.candidates[i].address);
-                var coffeeShopCard = $("<div>").addClass("col s12 m12");
-                var cardTitle = $("<h3>").addClass("header").text(dataEl.candidates[i].address);
+                let cityEl = dataEl.candidates[i].attributes.Place_addr;
+                console.log(dataEl.candidates[i].attributes.Place_addr);
+                var coffeeShopCard = $("<div>").addClass("col s12 m12").attr("id", "card-button");
+                var cardTitle = $("<h5>").addClass("header").text(dataEl.candidates[i].address);
                 var horizontalCard = $("<div>").addClass("card horizontal");
                 var cardImg = $("<div>").addClass("card-image");
                 var Img = $('<img />', {
@@ -56,13 +58,13 @@ const getCoffeeShops = () => {
                     alt: 'placeholder'
                 });
                 var cardStack = $("<div>").addClass("card-stack");
-                var cardContent = $("<div>").addClass("card-content")
+                var cardContent = $("<div>").addClass("card-content ")
                 // var cardButton = $("<a />").addClass("waves-effect waves-light btn").text("button")
-                var cardIdentifier = $("<li>").text("Coffee Shop");
-                var cardStoreHours = $("<li>").text("6am : 9pm");
+                var cardIdentifier = $("<h6>").text("Coffee Shop");
+                var cardStoreHours = $("<h6>").text("6am : 9pm");
 
-                $(coffeeShopCard).append(cardTitle);
                 $(coffeeShopCard).append(horizontalCard);
+                $(horizontalCard).append(cardTitle);
                 $(horizontalCard).append(cardStack);
                 $(cardStack).append(cardContent);
                 $(cardContent).append(cardIdentifier);
@@ -77,6 +79,31 @@ const getCoffeeShops = () => {
             }
             console.log(dataEl.candidates[1].address)
 
+
+
+
+            $("#card-button").click(function () {
+
+
+                L.mapquest.key = 'QR7nQvmiQcuP7wcQSNDMp8gjLvJsXBcr';
+
+                var map = L.mapquest.map('map', {
+                    center: [lon, lat],
+                    layers: L.mapquest.tileLayer('map'),
+                    zoom: 13
+                });
+                console.log(map)
+                var cityEl = 'murray';
+                L.mapquest.directions().route({
+                    start: '350 5th Ave, New York, NY 10118',
+                    end: cityEl
+
+                });
+                console.log(cityEl)
+            })
+
+
+
         })
 
     }
@@ -88,29 +115,12 @@ const getCoffeeShops = () => {
 
 
 
+
 }
 
 window.onload = () => {
     getCoffeeShops()
 }
-// var queryMapUrl = '';
-// var apiKey = '';
-// var submit = $(".submit");
-
-// window.onload = function () {
-//     L.mapquest.key = 'QR7nQvmiQcuP7wcQSNDMp8gjLvJsXBcr';    
-
-//     var map = L.mapquest.map('map', {
-//         center: [40.7128, -74.0059],    
-//         layers: L.mapquest.tileLayer('map'),
-//         zoom: 13
-//     });
-//     var hello = 'murray';
-//     L.mapquest.directions().route({
-//         start: '350 5th Ave, New York, NY 10118',    
-//         end: hello
-//     });
-// }
 
 
 // $.ajax({
